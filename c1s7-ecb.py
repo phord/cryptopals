cf78237405250d62b507776346d678cfc275c9ed
Phil Hord <phord@purestorage.com>
2017-04-05 11:52:56 -0700

    Add AES decrypt and finish challenge 7

--- c1s7-ecb.py
+++ c1s7-ecb.py
@@ -10,18 +10,5 @@ b64 = ''.join([line.rstrip('\n') for line in open('7.txt')])
 enc = b642bin(b64)
 key = txt2bin('YELLOW SUBMARINE')
 
-for x in blockSplit(expandKey([0]*16), 16):
-    print bin2hex(x)
-
-print
-
-def matrix_print(mat):
-    for x in mat:
-        print ' '.join(blockSplit(bin2hex(x),2))
-    print "  --"
-
-plaintext = txt2bin('Two One Nine Two')
-password = txt2bin('Thats my Kung Fu')
-
-vec = aes_encrypt(plaintext, password)
-print ' '.join(blockSplit(bin2hex(vec),2))
+dvec = aes_decrypt(enc, key)
+print bin2str(dvec),
